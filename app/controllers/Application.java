bfdc99f4e9c5bb6eedc6f6e42a50c22a1a7392c8
Rui Rocha <ruigrandaorocha@gmail.com>
2014-10-07 11:18:12 +0100

    Optimized imports. Removed some deprecated debug code. Added sql files to .gitignore.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,15 +1,11 @@
 package controllers;
 
 import models.User;
-import play.*;
 import play.data.Form;
-import play.db.ebean.Model;
 import play.db.ebean.Transactional;
 import play.mvc.*;
 
 import views.html.*;
-
-import java.util.List;
 
 import static play.libs.Json.toJson;
 
@@ -22,8 +18,6 @@ public class Application extends Controller {
     @Transactional
     public static Result addUser() {
         Form<User> regForm = Form.form(User.class).bindFromRequest();
-
-        System.out.println("Form: " + regForm.toString());
 
         if(regForm.hasErrors()){
             return badRequest("/registration");
